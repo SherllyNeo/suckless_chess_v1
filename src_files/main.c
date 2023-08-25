9b55833bce10e81727e0c8cee9caa519d0c5b5f7
sherlly <jacob@sherllymail.com>
2023-08-25 22:39:24 +0100

    fixed undo

--- src_files/main.c
+++ src_files/main.c
@@ -130,7 +130,11 @@ int main(int argc, char *argv[])
                 if (CheckCollisionPointRec(mousePoint,chess_square_as_rec) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                     squareState[x][y] = 1;
                     if (hand_buffer != NONE)
+                    {
                         change_flag = 1;
+                        if (history_index < history_len)
+                            history_len = history_index;
+                    }
                 }
                 else if (CheckCollisionPointRec(mousePoint,chess_square_as_rec) && IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
                     if (chess_board[x][y].piece != NONE)
